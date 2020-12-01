5a5a5314fa63f77896aeabe04e116902829c843a
stradivarius96 <matthew.pavan@gmail.com>
2020-12-01 17:01:41 +0000

    dec1.1 solved

---
+++ dec1.1.js
@@ -0,0 +1,28 @@
+var fs = require('fs');
+var array = fs.readFileSync('dec1.1-input.txt').toString().split("\n").map(Number).sort((a, b) => a - b);
+
+// Find 2 elements that sum to 2020 and multiply
+// Sort array and set pointer at start and end
+// If sum is below target, walk start pointer up
+// If sum is above target, walk top pointer down
+
+var start = 0
+var end = array.length - 1
+var done = false;
+var counter = 0;
+
+
+
+while (!done) {
+    counter++
+    if (array[start] + array[end] == 2020) {
+        console.log("Success: " + array[start] + " * " + array[end] + " = " + array[start] * array[end])
+        done = true
+    } else if (array[start] + array[end] > 2020) {
+        end--
+    } else {
+        start++
+    }
+}
+console.log("Counter: " + counter)
+
