cf8dbcaf11c92d22c42317bf7dab0e840d0b9614
stradivarius96 <matthew.pavan@gmail.com>
2020-12-01 17:07:47 +0000

    dec1.2 solved

--- dec1.2.js
+++ dec1.2.js
@@ -5,9 +5,11 @@ var array = fs.readFileSync('dec1.1-input.txt').toString().split("\n").map(Numbe
 // Sort array and set pointer at start and end
 // If sum is below target, walk start pointer up
 // If sum is above target, walk top pointer down
+// If no success, increment mid
 
 var start = 0
 var end = array.length - 1
+var mid = 1
 var done = false;
 var counter = 0;
 
@@ -15,13 +17,20 @@ var counter = 0;
 
 while (!done) {
     counter++
-    if (array[start] + array[end] == 2020) {
-        console.log("Success: " + array[start] + " * " + array[end] + " = " + array[start] * array[end])
+    if (start == end) {
+        start = 0
+        end = array.length - 1
+        mid++
+    }
+    if (array[start] + array[end] + array[mid] == 2020) {
+        console.log("Success: " + array[start] + " * " + array[end] + " * " + array[mid]
+            + " = " + array[start] * array[end] * array[mid])
         done = true
-    } else if (array[start] + array[end] > 2020) {
+    } else if (array[start] + array[end] + array[mid] > 2020) {
         end--
     } else {
         start++
     }
+
 }
 console.log("Counter: " + counter)
