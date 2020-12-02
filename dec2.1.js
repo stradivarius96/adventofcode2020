d84ae2d58adcf123ad2ff17f34f4c894636ef9ed
stradivarius96 <matthew.pavan@gmail.com>
2020-12-02 16:44:59 +0000

    dec2 solutions

---
+++ dec2.1.js
@@ -0,0 +1,17 @@
+var fs = require('fs');
+var arr = fs.readFileSync('dec2.1-input.txt').toString().split("\n")
+console.log(arr[0].split(' '))
+
+var valid = 0
+
+arr.forEach(row => {
+      var eles = row.split(' ')
+
+      var reg = "^[^" + eles[1].substr(0,1) +"]*(" +eles[1].substr(0,1) + "[^" + eles[1].substr(0,1) + "]*){" + eles[0].split('-')[0] + "," + eles[0].split('-')[1] + "}$"
+      console.log( reg + ' ' + eles[2]  + ' ' + RegExp(reg).test(eles[2]))
+      if (RegExp(reg).test(eles[2]))
+      {
+          valid++
+      }
+})
+console.log("Valid: " + valid)
---
+++ dec2.2.js
@@ -0,0 +1,15 @@
+var fs = require('fs');
+var arr = fs.readFileSync('dec2.1-input.txt').toString().split("\n")
+console.log(arr[0].split(' '))
+
+var valid = 0
+
+arr.forEach(row => {
+      var eles = row.split(' ')
+
+    if(!!(eles[2][eles[0].split('-')[0]-1] == eles[1].substr(0,1)) ^ (eles[2][eles[0].split('-')[1]-1] == eles[1].substr(0,1))){
+        valid++
+    }
+  
+})
+console.log("Valid: " + valid)
